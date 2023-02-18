ae5a64b3fbe6cf8d09cd8dba184790b921e29f4d
keayasimp <120783701+keayasimp@users.noreply.github.com>
2023-02-18 11:19:11 +0300

    Update settings.py

--- weatherprogect/settings.py
+++ weatherprogect/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xec^292^&!l$tc!$6s$^cs-xi9bay)-()wwcj@ybe!dp8&+(91"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
